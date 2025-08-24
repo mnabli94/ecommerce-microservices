03e925215054b9c25d4e43bcb9c2503af870c16a
mnabli <moez.nebli.94@gmail.com>
2025-08-24 17:02:35 +0200

    test(product): #0007 add @WebMvcTest for Product/Category

---
+++ product-service/src/test/java/com/demo/product/controller/CategoryControllerTest.java
@@ -0,0 +1,62 @@
+package com.demo.product.controller;
+
+import com.demo.product.dto.CategoryDTO;
+import com.demo.product.mapper.CategoryMapper;
+import com.demo.product.model.Category;
+import com.demo.product.service.CategoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryController.class)
+class CategoryControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    CategoryService service;
+
+    @Test
+    void create_shouldReturnCreated() throws Exception {
+        var dto = new CategoryDTO(null, "Electronics");
+        when(service.create(dto)).thenReturn(CategoryMapper.toDto(new Category(1L, "Electronics")));
+
+        mvc.perform(
+                        post("/categories")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("1"));
+    }
+
+    @Test
+    void create_shouldThrowError_whenNameIsEmpty() throws Exception {
+        mvc.perform(
+                        post("/categories")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {"name": ""}
+                                        """))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void get_shouldReturn404_whenMissing() throws Exception {
+        when(service.find(99L)).thenThrow(new EntityNotFoundException("not found"));
+        mvc.perform(get("/products/99"))
+                .andExpect(status().is(500));
+    }
+}
---
+++ product-service/src/test/java/com/demo/product/controller/ProductControllerTest.java
@@ -0,0 +1,71 @@
+package com.demo.product.controller;
+
+import com.demo.product.dto.ProductDTO;
+import com.demo.product.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.math.BigDecimal;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    ProductService service;
+
+    @Test
+    void create_shouldReturnSuccess() throws Exception {
+        var dto = new ProductDTO(1L, "Laptop", new BigDecimal("19.90"), true, 10L);
+        when(service.create(any())).thenReturn(dto);
+        mvc.perform(
+                post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("1"));
+    }
+
+    @Test
+    void create_shouldThrowError_whenNameIsMissing() throws Exception {
+        mvc.perform(
+                        post("/products")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {"price":19.90,"inStock":true,"categoryId":10}
+                                        """))
+                .andExpect(status().isBadRequest());
+    }
+
+    //TODO #0006 Need to add ExceptionHandler to perform exact tests
+//    @Test
+//    void get_shouldReturn404_whenMissing() throws Exception {
+//        when(service.find(99L)).thenThrow(new EntityNotFoundException("not found"));
+//        mvc.perform(get("/products/99"))
+//                .andExpect(status().isBadRequest());
+//    }
+
+//
+//    @Test
+//    void update() {
+//    }
+//
+//    @Test
+//    void delete() {
+//    }
+}
