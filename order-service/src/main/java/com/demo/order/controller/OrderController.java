b99e8eafa8abf9f3a5313f837fb2bd727f27645d
mnabli <moez.nebli.94@gmail.com>
2025-09-03 03:41:31 +0200

    feat(order): add GET methods

--- order-service/src/main/java/com/demo/order/controller/OrderController.java
+++ order-service/src/main/java/com/demo/order/controller/OrderController.java
@@ -4,11 +4,16 @@ import com.demo.order.dto.in.OrderInDTO;
 import com.demo.order.dto.out.OrderOutDTO;
 import com.demo.order.service.OrderService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -19,8 +24,34 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderOutDTO> get(@NotNull @PathVariable UUID id){
+        return ResponseEntity.ok(orderService.find(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<OrderOutDTO>> get(@RequestParam(name = "page", defaultValue = "0") int page,
+                                                 @RequestParam(name = "size", defaultValue = "10") int size,
+                                                 @RequestParam(name = "sortBy", defaultValue = "createdAt") String sortBy,
+                                                 @RequestParam(name = "direction", defaultValue = "asc") String direction){
+        Sort sort = direction.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page,size).withSort(sort);
+        return ResponseEntity.ok(orderService.findAll(pageable));
+    }
+
     @PostMapping
     public ResponseEntity<OrderOutDTO> createOrder(@Valid @RequestBody OrderInDTO dto) {
-        return ResponseEntity.ok(orderService.createOrder(dto));
+        var out = orderService.createOrder(dto);
+        return ResponseEntity
+                .created(URI.create("/orders" + out.id()))
+                .body(out);
     }
+
+    @PatchMapping("/{id}/confirm")
+    public OrderOutDTO confirm(@PathVariable UUID id) { return orderService.confirm(id); }
+
+    @PatchMapping("/{id}/cancel")
+    public OrderOutDTO cancel(@PathVariable UUID id) { return orderService.cancel(id); }
 }
--- order-service/src/main/java/com/demo/order/service/OrderService.java
+++ order-service/src/main/java/com/demo/order/service/OrderService.java
@@ -11,11 +11,14 @@ import com.demo.order.mapper.OrderMapper;
 import com.demo.order.repository.OrderRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -37,7 +40,6 @@ public class OrderService {
 
     @Transactional
     public OrderOutDTO createOrder(OrderInDTO dto) {
-
         Order order = orderMapper.toEntity(dto);
         order.setCreatedAt(LocalDateTime.now());
         order.setStatus(OrderStatus.PENDING);
@@ -46,30 +48,19 @@ public class OrderService {
             setUpProduct(item);
             item.setOrder(order);
         });
-
         order.calculateTotalAmount();
         Order saved = orderRepository.save(order);
+        return getOrderOutDTOWithProductDetails(saved);
+    }
 
-        OrderOutDTO result = orderMapper.toOutDto(saved);
+    public OrderOutDTO find(UUID id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Order not found with id: %s".formatted(id)));
+        return getOrderOutDTOWithProductDetails(order);
+    }
 
-        Executor executor = Executors.newFixedThreadPool(16);
-        List<CompletableFuture<ProductDTO>> futures = saved.getOrderItems().stream()
-                .map(item -> Long.parseLong(item.getProductId()))
-                .map(id -> CompletableFuture.supplyAsync(() -> productClient.getProduct(id), executor)) // Executor pour threads
-                .toList();
-        List<ProductDTO> products = futures.stream().map(CompletableFuture::join).toList();
-
-        var finalOrderItems = saved.getOrderItems().stream().map(item -> {
-            var pid = Long.valueOf(item.getProductId());
-            var product = products.stream()
-                    .filter(productDTO -> Objects.equals(productDTO.id(), pid))
-                    .findFirst().orElseThrow(() -> new RuntimeException("product with id=%d is not more available"));
-            var itemDto = orderMapper.toOutDto(item);
-            return new OrderItemOutDTO(itemDto.id(), product, itemDto.quantity(), itemDto.unitPrice());
-        }).toList();
-
-        return new OrderOutDTO(result.id(), result.status(), result.shippingAddress(), finalOrderItems, result.totalAmount());
-
+    public Page<OrderOutDTO> findAll(Pageable pageable) {
+        return orderRepository.findAll(pageable).map(this::getOrderOutDTOWithProductDetails);
     }
 
     private void setUpProduct(OrderItem item) {
@@ -91,6 +82,50 @@ public class OrderService {
         if (product.price() == null) {
             throw new RuntimeException("Product price missing: " + productId);
         }
+    }
+
+
+    @Transactional
+    public OrderOutDTO confirm(UUID id) {
+        var order = orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        if (order.getStatus() != OrderStatus.PENDING) {
+            throw new IllegalStateException("Only PENDING orders can be confirmed");
+        }
+        order.setStatus(OrderStatus.CONFIRMED);
+        var saved = orderRepository.save(order);
+        return getOrderOutDTOWithProductDetails(saved);
+    }
+
+    @Transactional
+    public OrderOutDTO cancel(UUID id) {
+        var order = orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        if (order.getStatus() == OrderStatus.CONFIRMED) {
+            throw new IllegalStateException("CONFIRMED orders cannot be cancelled");
+        }
+        order.setStatus(OrderStatus.CANCELLED);
+        var saved = orderRepository.save(order);
+        return getOrderOutDTOWithProductDetails(saved);
+    }
+
+    private OrderOutDTO getOrderOutDTOWithProductDetails(Order saved) {
+        OrderOutDTO result = orderMapper.toOutDto(saved);
+        Executor executor = Executors.newFixedThreadPool(16);
+        List<CompletableFuture<ProductDTO>> futures = saved.getOrderItems().stream()
+                .map(item -> CompletableFuture.supplyAsync(() ->
+                        productClient.getProduct(Long.parseLong(item.getProductId())), executor))
+                .toList();
+        List<ProductDTO> products = futures.stream().map(CompletableFuture::join).toList();
+
+        var finalOrderItems = saved.getOrderItems().stream().map(item -> {
+            var pid = Long.valueOf(item.getProductId());
+            var product = products.stream()
+                    .filter(productDTO -> Objects.equals(productDTO.id(), pid))
+                    .findFirst().orElseThrow(() -> new RuntimeException("product with id=%d is not more available"));
+            var itemDto = orderMapper.toOutDto(item);
+            return new OrderItemOutDTO(itemDto.id(), product, itemDto.quantity(), itemDto.unitPrice());
+        }).toList();
+
+        return new OrderOutDTO(result.id(), result.status(), result.shippingAddress(), finalOrderItems, result.totalAmount());
     }
 
 }
